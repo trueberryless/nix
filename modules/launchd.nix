181fcabfe1300892ba279bdd018a6fb887070db7
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-01-17 11:49:41 +0100

    feat: spotify and clipbook start always

--- modules/launchd.nix
+++ modules/launchd.nix
@@ -2,7 +2,6 @@
   launchd.user.agents.clipbook = {
     serviceConfig = {
       ProgramArguments = [ "/Applications/Clipbook.app/Contents/MacOS/Clipbook" ];
-      KeepAlive = true;
       RunAtLoad = true;
       ProcessType = "Interactive";
     };
